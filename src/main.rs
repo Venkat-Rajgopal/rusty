88f6a16df89c4d0b18059a738779df6e70b1e802
Venkatramani Rajgopal <venkatramani.r@gmail.com>
2023-10-13 17:31:17 +0000

    :sparkles: add basic rust setup

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+fn test(_a:i32) {
+    
+    if _a % 2 == 0{
+        print!("Number {} is even", _a);
+      }
+      else {
+        print!("Number {} is odd", _a);
+    
+}
+}
+
+
+fn main(){
+    test(5);
+}
